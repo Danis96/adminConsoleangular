a1bb6184c42834263e2028b62b9887d5a7ee96a8
Elvir9 <ehalilovicc@gmail.com>
2020-08-12 12:52:50 +0200

    angularMaterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
