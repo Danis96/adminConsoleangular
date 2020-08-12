d15ee5d029933b051a7587d5e43c87cbe1ac0ede
Amer Hero <amherhero@gmail.com>
2020-08-12 15:10:12 +0200

    added customers list

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -9,6 +9,21 @@ export class CardComponent implements OnInit {
 
   constructor() { }
 
+  listaCustomera= [
+    {
+      title:"Danis Preldzic",
+      city:"Sarajevo, Bosna i Hercegovina"
+    },
+    {
+      title:"Elvir Halilovic",
+      city:"Sarajevo, Bosna i Hercegovina"
+    },
+    {
+      title:"Amer Hero",
+      city:"Sarajevo, Bosna i Hercegovina"
+    }
+  ]
+
   ngOnInit(): void {
   }
 
